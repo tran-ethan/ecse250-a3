2926d6db9dd83da062f55e49d17c3b7e9dbdcb6a
tran-ethan <tran.ethan@hotmail.com>
2024-11-30 16:45:14 -0500

    perimiter goal score

--- src/main/java/assignment3/PerimeterGoal.java
+++ src/main/java/assignment3/PerimeterGoal.java
@@ -10,10 +10,29 @@ public class PerimeterGoal extends Goal{
 
 	@Override
 	public int score(Block board) {
-		/*
-		 * ADD YOUR CODE HERE
-		 */
-		return 0;
+		Color[][] grid = board.flatten();
+		int size = grid.length;
+		int score = 0;
+
+		for (int i = 0; i < size; i++) {
+			// First row
+			if (grid[0][i].equals(targetGoal)) {
+				score++;
+			}
+			// Last row
+			if (grid[size-1][i].equals(targetGoal)) {
+				score++;
+			}
+			// First column
+			if (grid[i][0].equals(targetGoal)) {
+				score++;
+			}
+			// Last column
+			if (grid[i][size-1].equals(targetGoal)) {
+				score++;
+			}
+		}
+		return score;
 	}
 
 	@Override
